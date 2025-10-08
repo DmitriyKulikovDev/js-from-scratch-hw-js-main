344d5d8d216bcd772959a0fa60c9681b51083054
Dmitriy Kulikov <DKulikov60@gmail.com>
2025-10-08 21:50:01 +0300

    complete lesson 3 task 3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,16 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(n1, n2, n3) {
+    if (n1 > n2 && n1 > n3) {
+        console.log("Число " + n1 + " - наибольшее");
+        return n1        
+    } else if (n2 > n1 && n2 > n3) {
+        console.log("Число " + n1 + " - наибольшее");
+        return n2
+    } else if (n3 > n1 && n3 > n1) {
+        console.log("Число " + n1 + " - наибольшее");
+        return n1
+    } 
+}
+
+findLargest(111, 89, 120)

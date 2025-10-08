436d2a1a7f08d38c35bef06c93060a7818a99d5f
Dmitriy Kulikov <DKulikov60@gmail.com>
2025-10-08 21:54:36 +0300

    complete lesson 3 task 3 v2

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -5,11 +5,11 @@ function findLargest(n1, n2, n3) {
         console.log("Число " + n1 + " - наибольшее");
         return n1        
     } else if (n2 > n1 && n2 > n3) {
-        console.log("Число " + n1 + " - наибольшее");
+        console.log("Число " + n2 + " - наибольшее");
         return n2
-    } else if (n3 > n1 && n3 > n1) {
-        console.log("Число " + n1 + " - наибольшее");
-        return n1
+    } else if (n3 > n1 && n3 > n2) {
+        console.log("Число " + n3 + " - наибольшее");
+        return n3
     } 
 }
 

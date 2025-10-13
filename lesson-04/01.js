8f37fbc5993f48fe897e92ef845b3d83befadaba
Dmitriy Kulikov <DKulikov60@gmail.com>
2025-10-13 20:21:58 +0300

    complete lesson 4 task 01

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,35 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+const arr = [32,11,33,12,54,22,47];
+
+function includesElement(someArray, element) {
+    const includedElement = [];
+    let isFound;
+    for (let i = 0; i < someArray.length; i++) {
+        
+        if (element === someArray[i]) {
+            isFound = true;
+            includedElement.push(element); // продумать как вывести найденный элемент
+            return isFound
+        } else {
+            isFound = false;
+        }
+        
+    }
+    return isFound
+
+    
+}
+
+const number = 48;  // искомое число присваиваем переменной, чтобы можно было ее выводить
+const foundElement = includesElement(arr, number);
+// выводим нужное сообщение в зависимости от того, найдено число или нет
+// код ниже можно вывести в функции
+
+if (foundElement === true) {            // если наша функция возвращает true, то выводим сообщение о том, что число найдено
+    console.log("Number " + number + " is found");
+    
+} else {                                    // в обратном случае -  не найдено
+    console.log("Number " + number + " is NOT found");
+}

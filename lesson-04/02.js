c7a087755cb4554cea3a7ab16289d517f4ddf23e
Dmitriy Kulikov <DKulikov60@gmail.com>
2025-10-14 21:10:47 +0300

    complete lesson 4 task 02

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -14,5 +14,32 @@
 
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
+const newArr = [7,1,30,8,2,1,9,7,9]
+//const num = 34;
+//const newFoundElement = includesElement(newArr, num);
+//
 
-function findUniqueElements() {}
+function findUniqueElements(someArray) {
+    const uniqueElement = [];
+    for (let i = 0; i < someArray.length; i++) {
+           const num = someArray[i]; 
+           const foundEl = includesElement(uniqueElement, num);
+           if (i === 0) {           
+            uniqueElement.push(num)                 //Первый элемент всегда уникальный - всегда включаем в массив
+           } else if (foundEl === false) {      //рефакторинг
+            uniqueElement.push(num)
+           }
+    }
+    console.log(uniqueElement);
+}
+
+findUniqueElements(newArr);
+
+
+
+
+// const num = someArray[i];
+//         const foundEl = includesElement(newArr, num)
+//             if (foundEl === false) {     // если данного элемента нет, 
+//                  uniqueElement.push(num)                       // то мы его добавляем в новый массив (уникальный)
+//             }

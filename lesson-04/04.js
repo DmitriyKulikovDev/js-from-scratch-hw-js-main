c7783e245187993863fbdef24918096e8520b042
Dmitriy Kulikov <DKulikov60@gmail.com>
2025-10-25 19:15:44 +0300

    lesson 4 task 04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,18 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+const Str = 'hello'
+function doubleEachCharacter(inputString) {
+    const arr = inputString.split('');
+    const newStr = []
+        for (let i = 0; i < arr.length; i++) {
+            const el = arr[i];
+            newStr.push(el + el)    //так и плачет рефыакторинг
+            
+        }
+    let resultString = newStr.join('');
+    return resultString
+}
+
+const Finalstr = doubleEachCharacter(Str);
+console.log(Finalstr);
